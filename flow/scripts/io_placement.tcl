ff0c9452a740136bbcb05d96cf2bc560f907adc3
Augusto Berndt <augusto.berndt@precisioninno.com>
2026-03-11 11:02:54 +0000

    only call report_design_area if design is loaded
    Signed-off-by: Augusto Berndt <augusto.berndt@precisioninno.com>

--- flow/scripts/io_placement.tcl
+++ flow/scripts/io_placement.tcl
@@ -11,10 +11,11 @@ if {
     -hor_layers $::env(IO_PLACER_H) \
     -ver_layers $::env(IO_PLACER_V) \
     {*}[env_var_or_empty PLACE_PINS_ARGS]
+  report_design_area
+
   orfs_write_db $::env(RESULTS_DIR)/3_2_place_iop.odb
   write_pin_placement $::env(RESULTS_DIR)/3_2_place_iop.tcl
 } else {
   log_cmd exec cp $::env(RESULTS_DIR)/3_1_place_gp_skip_io.odb $::env(RESULTS_DIR)/3_2_place_iop.odb
 }
 
-report_design_area
